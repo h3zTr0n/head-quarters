1aeb810134166e648dbb36ec957fcf4f2bd60ccc
h3zTr0n <alison.mukoma@yahoo.com>
2017-09-16 16:45:52 +0200

    successfully built dpcker containers

--- config/settings/local.py
+++ config/settings/local.py
@@ -46,7 +46,8 @@ CACHES = {
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 INSTALLED_APPS += ['debug_toolbar', ]
 
-INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', '0.0.0.0']
+ALLOWED_HOSTS =['127.0.0.1', '0.0.0.0']
 
 
 import socket
